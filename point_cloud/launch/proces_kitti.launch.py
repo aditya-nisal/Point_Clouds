d5cf75a660ce9824abafc3f3a74ddb7c57ba06b0
anisal-monarch <anisal@wpi.edu>
2023-09-23 21:24:00 -0700

    lanuch file for PCD rviz

---
+++ point_cloud/launch/proces_kitti.launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+import os
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        Node(
+            package='ros2_kitti_publishers', executable = 'kitti_publishers', output = 'screen',
+        ),
+
+        # Node(
+        #     package='point_cloud', executable = 'segment', output = 'screen',
+        # ),
+    ])
